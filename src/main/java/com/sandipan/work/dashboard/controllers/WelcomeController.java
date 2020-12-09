01c31e5dc005e3bcbd252959d7087ea552100607
sandy007sandipan <$Passw0rd$>
2020-12-09 16:53:11 +0530

    Adding Skeleton Code to GIT - Set 2

--- src/main/java/com/sandipan/work/dashboard/controllers/WelcomeController.java
+++ src/main/java/com/sandipan/work/dashboard/controllers/WelcomeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dashboard")
-@CrossOrigin("http://localhost:3000")
 public class WelcomeController {
 
     @GetMapping("/getMessage")
