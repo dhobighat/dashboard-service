e0da4d4375b22d716cb08ea1f30542203e80b6f6
sandy007sandipan <$Passw0rd$>
2020-12-22 13:02:07 +0530

    Adding Skeleton Code to GIT - Set 2

---
+++ src/test/java/com/sandipan/work/dashboard/DashboardServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sandipan.work.dashboard;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardServiceApplicationTests {
+    @Test
+	void contextLoads() {
+	}
+
+}
