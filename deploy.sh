9418cd18bdf60b7ccaa67d1079d836a719854595
sandy007sandipan <$Passw0rd$>
2021-01-09 18:24:18 +0530

    Travis CI Build config

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# install AWS SDK
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+# install necessary dependency for ecs-deploy
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+
+# install ecs-deploy
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
+  sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+
+# update an AWS ECS service with the new image
+ecs-deploy -c aws-dev-ecs-cluster -n dashboard-service -i dashboard-service:latest
